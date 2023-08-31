f755223718a21096c815fa159a47262bbe692c70
Aaron Spiegel <spiegela@gmail.com>
2023-08-31 06:40:59 -0500

    Update to use GST Livkeit plugin & multiple tracks

--- src/main.rs
+++ src/main.rs
@@ -1,201 +1,60 @@
 use std::env;
-use std::sync::{Arc, Mutex};
-use std::time::Duration;
 
-use gstreamer::{element_error, ElementFactory, Pipeline, ReferenceTimestampMeta, State};
-use gstreamer::prelude::{Cast, ElementExt, ElementExtManual, FormattedValue, GstBinExtManual, GstObjectExt};
-use gstreamer_app::{AppSink, AppSinkCallbacks, gst};
-use livekit::options::{AudioCaptureOptions, TrackPublishOptions, VideoCaptureOptions};
-use livekit::prelude::*;
-use livekit::webrtc::{
-    audio_frame::AudioFrame,
-    audio_source::native::NativeAudioSource,
-    video_frame::{I420Buffer, VideoFrame, VideoRotation},
-    video_frame::native::I420BufferExt, video_source::native::NativeVideoSource,
-};
-use livekit_api::access_token::{AccessToken, VideoGrants};
-
-use byteorder::{BigEndian, ReadBytesExt};
+use gstreamer::{Element, parse_bin_from_description, parse_bin_from_description_full, ParseFlags, Pipeline, State};
+use gstreamer::prelude::{Cast, ElementExt, ElementExtManual, GstBinExt, GstObjectExt};
+use gstreamer::prelude::PadExt;
 use gstreamer::State::Null;
+use gstreamer_rtp::gst;
 
 #[tokio::main]
 async fn main() {
-    let livekit_url = env::var("LIVEKIT_WSS_URL").unwrap();
-    let livekit_token = AccessToken::new()
-        .unwrap()
-        .with_ttl(Duration::from_secs(3600))
-        .with_identity("gstreamer")
-        .with_name("gstreamer")
-        .with_grants(VideoGrants {
-            room_join: true,
-            can_publish: true,
-            can_publish_data: true,
-            can_publish_sources: vec!["Camera".to_string(), "Microphone".to_string()],
-            can_update_own_metadata: true,
-            room: "Default".to_string(),
-            ..Default::default()
-        })
-        .to_jwt().unwrap();
-
-    let (room, _event_ch) = Room::connect(
-        livekit_url.as_str(),
-        livekit_token.as_str(),
-    ).await.unwrap();
-    let video_source = NativeVideoSource::default();
-    let video_track = LocalVideoTrack::create_video_track(
-        "gstreamer-test",
-        VideoCaptureOptions::default(),
-        video_source.clone(),
-    );
-    let audio_source = NativeAudioSource::default();
-    let audio_track = LocalAudioTrack::create_audio_track("gstreamer-test", AudioCaptureOptions {
-        echo_cancellation: true,
-        auto_gain_control: true,
-        noise_suppression: true,
-    }, audio_source.clone());
-    let session = room.session();
-    session.local_participant()
-        .publish_track(
-            LocalTrack::Video(video_track.clone()),
-            TrackPublishOptions { source: TrackSource::Camera, ..Default::default() },
-        )
-        .await.unwrap();
-    session
-        .local_participant()
-        .publish_track(
-            LocalTrack::Audio(audio_track.clone()),
-            TrackPublishOptions { source: TrackSource::Microphone, ..Default::default() },
-        )
-        .await.unwrap();
-    tokio::task::spawn_blocking(|| unsafe {
-        track_task(video_source, audio_source)
-    }).await.unwrap().await;
-}
-
-async unsafe fn track_task(video_source: NativeVideoSource, audio_source: NativeAudioSource) {
-    let (height, width) = (720, 1280);
+    let url = env::var("LIVEKIT_URL").unwrap();
+    let api_key = env::var("LIVEKIT_API_KEY").unwrap();
+    let api_secret = env::var("LIVEKIT_API_SECRET").unwrap();
     gst::init().unwrap();
-    let videotestsrc = ElementFactory::make("videotestsrc")
-        .build().unwrap();
-    let vid_appsink = AppSink::builder()
-        .caps(&gst::Caps::builder("video/x-raw")
-            .field("format", gstreamer_video::VideoFormat::I420.to_str())
-            .field("width", width as i32)
-            .field("height", height as i32)
-            .build(),
-        ).build();
 
-    let vid_frame = Arc::new(Mutex::new(VideoFrame {
-        rotation: VideoRotation::VideoRotation0,
-        timestamp: 0,
-        buffer: I420Buffer::new(1280, 720),
-    }));
-
-    vid_appsink.set_callbacks(
-        AppSinkCallbacks::builder()
-            .new_sample(move |appsink| {
-                let sample = appsink.pull_sample().map_err(|_| gst::FlowError::Eos)?;
-                let buffer = sample.buffer().ok_or_else(|| {
-                    element_error!(
-                        appsink,
-                        gst::ResourceError::Failed,
-                        ("Failed to get buffer from appsink")
-                    );
-                    gst::FlowError::Error
-                })?;
-                let mut frame = vid_frame.lock().unwrap();
-                let i420_buffer = &mut frame.buffer;
-                let (dst_y, dst_u, dst_v) = i420_buffer.data_mut();
-                let mut ts = 0;
-                if let Some(meta) = buffer.meta::<ReferenceTimestampMeta>() {
-                    ts = meta.timestamp().into_raw_value();
-                }
-                let caps = sample.caps().unwrap();
-                let video_info = gstreamer_video::VideoInfo::from_caps(caps).unwrap();
-                let sample_frame =
-                    gstreamer_video::video_frame::VideoFrame::from_buffer_readable(
-                        buffer.copy(),
-                        &video_info,
-                    ).unwrap();
-                let src_y = sample_frame.plane_data(0).unwrap();
-                let src_u = sample_frame.plane_data(1).unwrap();
-                let src_v = sample_frame.plane_data(2).unwrap();
-                dst_y.copy_from_slice(src_y);
-                dst_u.copy_from_slice(src_u);
-                dst_v.copy_from_slice(src_v);
-                frame.timestamp = ts;
-
-                video_source.capture_frame(&*frame);
-                Ok(gst::FlowSuccess::Ok)
-            }).build(),
-    );
-
-    let audiotestsrc = ElementFactory::make("audiotestsrc")
-        .build().unwrap();
-    let aud_appsink = AppSink::builder()
-        .caps(&gst::Caps::builder("audio/x-raw")
-            .field("format", gstreamer_audio::AudioFormat::S16be.to_str())
-            .field("layout", "interleaved")
-            .field("rate", 48000)
-            .field("channels", 2)
-            .build(),
-        ).build();
-
-
-    aud_appsink.set_callbacks(
-        AppSinkCallbacks::builder()
-            .new_sample(move |appsink| {
-                let sample = appsink.pull_sample().map_err(|_| gst::FlowError::Eos)?;
-                let buffer = sample.buffer().ok_or_else(|| {
-                    element_error!(
-                        appsink,
-                        gst::ResourceError::Failed,
-                        ("Failed to get buffer from appsink")
-                    );
-                    gst::FlowError::Error
-                })?;
-                let audio_info = gstreamer_audio::AudioInfo::builder(gstreamer_audio::AudioFormat::S16be, 48000, 2)
-                    .build().unwrap();
-                let audio_buffer = gstreamer_audio::AudioBuffer::from_buffer_readable(
-                    buffer.copy(),
-                    &audio_info,
-                ).unwrap();
-                let map = buffer.map_readable().map_err(|_| {
-                    element_error!(
-                        appsink,
-                        gst::ResourceError::Failed,
-                        ("Failed to map buffer readable")
-                    );
-                    gst::FlowError::Error
-                })?;
-                let mut plane_data = map.as_slice();
-                let mut data = vec![0i16; audio_buffer.n_samples() * 2];
-                let data_slice = data.as_mut_slice();
-                let data = data_slice.to_vec();
-                println!("data_vec len: {:?}", data.len());
-                ReadBytesExt::read_i16_into::<BigEndian>(&mut plane_data, data_slice).unwrap();
-                audio_source.capture_frame(&AudioFrame {
-                    data,
-                    sample_rate: audio_buffer.rate(),
-                    num_channels: audio_buffer.channels(),
-                    samples_per_channel: audio_buffer.n_samples() as u32 / 2, // todo check size
-                });
-                Ok(gst::FlowSuccess::Ok)
-            }).build(),
-    );
-
-    let pipeline = Pipeline::new(None);
-    pipeline.add_many(&[
-        &videotestsrc,
-        vid_appsink.upcast_ref(),
-        &audiotestsrc,
-        aud_appsink.upcast_ref(),
-    ]).unwrap();
-
-    videotestsrc.link(&vid_appsink).unwrap();
-    audiotestsrc.link(&aud_appsink).unwrap();
-
+    let pipeline = Pipeline::new();
     pipeline.set_state(State::Playing).unwrap();
+
+    let sink = parse_bin_from_description_full(
+        format!(r#"livekitwebrtcsink
+            signaller::ws-url={url}
+            signaller::api-key={api_key}
+            signaller::secret-key={api_secret}
+            signaller::room-name=Default
+            signaller::identity=Gstreamer"#
+        ).as_str(),
+        false,
+        None,
+        ParseFlags::NO_SINGLE_ELEMENT_BINS,
+    ).expect("Unable to parse sink bin");
+
+
+    let pad_template = sink.pad_template("video_%u").expect("Unable to get sink pad template");
+    let patterns = vec!["smpte", "snow", "ball"];
+    let pads = patterns.iter().enumerate().map(|(i, _p)| {
+        let pad_name = format!("video_{}", i);
+        sink.request_pad(&pad_template, Some(pad_name.as_str()), None).expect("Unable to request sink_pad")
+    }).collect::<Vec<_>>();
+
+    pipeline.add(&sink).unwrap();
+    sink.sync_state_with_parent().unwrap();
+
+    for (i, p) in patterns.iter().enumerate() {
+        let src_text = format!(r#"videotestsrc pattern={} num-buffers=200000000 ! videoconvert ! video/x-raw ! queue"#, p);
+        let src_bin = parse_bin_from_description(
+            &src_text,
+            true,
+        ).expect("Unable to parse src bin");
+
+        let src_el: Element = src_bin.upcast();
+        pipeline.add(&src_el).expect("Unable to add src bin");
+
+        let src_pad = src_el.static_pad("src").expect("No matching static pad");
+        src_pad.link(&pads[i]).expect("Unable to link pads");
+
+        src_el.sync_state_with_parent().expect("Unable to sync src state with parent");
+    }
 
     let bus = pipeline
         .bus()
@@ -203,7 +62,6 @@ async unsafe fn track_task(video_source: NativeVideoSource, audio_source: Native
 
     for msg in bus.iter_timed(gst::ClockTime::NONE) {
         use gst::MessageView;
-
         match msg.view() {
             MessageView::Eos(..) => break,
             MessageView::Error(err) => {
